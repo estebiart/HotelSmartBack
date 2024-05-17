ae4a53282e73cda8f5b97f679eed57bfee397f25
Anderson Lozada <andersonlozada@flare.com.co>
2024-05-17 13:54:07 -0500

    build: build

--- routes/hotel.js
+++ routes/hotel.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Hotel = require("../schema/hotel");
 const multer = require("multer");
-const path = require("path");ç
+const path = require("path");
 const { BlobServiceClient } = require("@azure/storage-blob");
 require("dotenv").config();
 
